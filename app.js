b34052e53b8acbf21b7a5d1744601478f3347401
Alireza Minagar <127906160+aliminagar@users.noreply.github.com>
2025-10-29 14:17:01 -0500

    Change MongoDB connection string to use Atlas

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  mongoose.connect("mongodb+srv://user:password@cluster.mongodb.net/wtwr_db")
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
